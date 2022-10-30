3c48b1dc9ac5dcd2d61854d2743e706f5c9949dc
andreytodd <ermolaev@seznam.cz>
2022-10-30 12:54:48 +0100

    docs: updatex index.html and css, js

--- pages/main/script.js
+++ pages/main/script.js
@@ -10,3 +10,17 @@ let clicked = (data) => {
         }
     })
 }
+
+const submitBtn = document.getElementById('submit')
+
+const emailInp = document.getElementById('email')
+
+emailInp.onkeyup = () => {
+    if (!emailInp.checkValidity()) {
+        submitBtn.style.border = '1px solid #D31414'
+        submitBtn.style.color = '#D31414'
+    } else {
+        submitBtn.style.border = '1px solid black'
+        submitBtn.style.color = 'black'
+    }
+}
